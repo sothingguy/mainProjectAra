42ef18de45505ef95a605a7b01d35b6b08a99ff0
sothingguy <sothingguy@gmail.com>
2020-06-11 11:01:13 +1200

    Error page added

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello, Flask!"
+@app.errorhandler(404)
+def error404(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
