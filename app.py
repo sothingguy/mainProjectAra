56be68aa84cf2ced381f822d86dc2e3fd6eb88a6
sothingguy <sothingguy@gmail.com>
2020-06-18 11:50:08 +1200

    Changes before lunch

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template('home.html')
-#@app.route("/Base")
-#def base():
-    #return render_template('Base.html')
+
+@app.route("/Base")
+def base():
+    return render_template('Base.html')
 
 @app.errorhandler(404)
 def error404(e):
