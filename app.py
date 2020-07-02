179a6fb6361df9f0dab0822a5844c0b8aa8ddf65
sothingguy <sothingguy@gmail.com>
2020-07-02 13:00:52 +1200

    general quality of life changes

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
-def home():
-    return render_template('home.html')
+def Home():
+    return render_template('Home.html')
 
 @app.route("/Base")
 def base():
     return render_template('Base.html')
+
+@app.route("/Explore.html")
+def Explore():
+    return render_template('Explore.html')
 
 @app.errorhandler(404)
 def error404(e):
